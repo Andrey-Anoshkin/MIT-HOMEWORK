72a567aa9c5a61db51e1d7de6b72ebaf146dce60
Anoshkin Andrey <89372650223@mail.ru>
2022-03-08 22:24:34 +0300

    Added function fib + .gitignore file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int fib(int n) {
+	if (n <= 2)
+		return 1;
+
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
 	std::cout << "Hello, World!\n";
+
+	int n = 0; std::cout << "Input n = "; std::cin >> n; 
+	std::cout << fib(n) << "\n";
+
 	return 0;
 }
