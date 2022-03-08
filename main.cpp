3f34470a4624594a28ff03411926d64b9b291f98
Anoshkin Andrey <89372650223@mail.ru>
2022-03-08 22:28:38 +0300

    Made fib using iterators

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 
-int fib(int n) {
-	if (n == 1)
-		return 0;
-	if (n == 2)
-		return 1;
+void fib(int n) {
+	int a = 0, b = 1;
 
-	return fib(n - 1) + fib(n - 2);
+	for (int i = 0; i < n; ++i) {
+		std::cout << a << "\n";
+
+		int t = b;
+		b += a;
+		a = t;
+	}
 }
 
 int main() {
 	std::cout << "Hello, World!\n";
 
 	int n = 0; std::cout << "Input n = "; std::cin >> n; 
-	std::cout << fib(n) << "\n";
+	fib(n);
 
 	return 0;
 }
